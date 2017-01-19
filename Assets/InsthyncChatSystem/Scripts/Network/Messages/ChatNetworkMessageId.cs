6183293fad75cc56bd1700f3039b3627b6e6572f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-01-20 02:39:04 +0700

    Set message id by official's highest

--- Assets/InsthyncChatSystem/Scripts/Network/Messages/ChatNetworkMessageId.cs
+++ Assets/InsthyncChatSystem/Scripts/Network/Messages/ChatNetworkMessageId.cs
@@ -1,11 +1,13 @@
+using UnityEngine.Networking;
+
 namespace Insthync.ChatSystem
 {
     public class ChatNetworkMessageId
     {
         // Developer can changes these Ids to avoid hacking while hosting
-        public const short ToServerChatSendMsgId = 4000;
-        public const short ToServerChatLoginRequestMsgId = 4001;
-        public const short ToClientChatReceiveMsgId = 4002;
-        public const short ToClientChatLoginSuccessMsgId = 4003;
+        public const short ToServerChatSendMsgId = MsgType.Highest + 101;
+        public const short ToServerChatLoginRequestMsgId = MsgType.Highest + 102;
+        public const short ToClientChatReceiveMsgId = MsgType.Highest + 103;
+        public const short ToClientChatLoginSuccessMsgId = MsgType.Highest + 104;
     }
 }
